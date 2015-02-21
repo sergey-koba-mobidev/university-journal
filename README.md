cb37e26e60390c7aa0f505c146a169a50b5610a4
Sergey Koba <s.koba@mobidev.biz>
2015-02-21 17:46:51 +0200

    creating labs

--- README.md
+++ README.md
@@ -2,8 +2,11 @@
 A Ruby on Rails based university classes manager.
 
 Main features:
-- Users with 3 roles: admin, treacher, studen
+- Users with 3 roles: admin, teacher, student
 - Managing Semesters
 - Managing Disciplines
-- Managing groups of students
+- Managing Groups of students
 - Managing Labs, Lectures, Modules, Exams
+
+TODO:
+- statistics
--- app/controllers/visits_controller.rb
+++ app/controllers/visits_controller.rb
@@ -1,7 +1,28 @@
 class VisitsController < ApplicationController
   def create
+    @visit = Visit.new(visit_params)
+    if owner_or_admin(@visit.relationship.discipline) && @visit.save
+      redirect_to :back, notice: 'Successfully added!'
+    else
+      redirect_to :back, alert: 'Access denied!'
+    end
   end
 
   def destroy
+
+  end
+
+  private
+
+  def set_visit
+    @visit = Visit.find(params[:id])
+  end
+
+  def visit_params
+    params.require(:visit).permit(:relationship_id, :title, :type)
+  end
+
+  def owner_or_admin(discipline)
+    current_user.admin? || current_user.id == discipline.user_id
   end
 end
