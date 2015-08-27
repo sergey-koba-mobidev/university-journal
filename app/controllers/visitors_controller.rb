adea0d81702ca7ab7e7b1cf2ed186c571170d4d2
Sergey Koba <s.koba@mobidev.biz>
2015-08-27 21:09:32 +0300

    fix current semester bug

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,6 +1,6 @@
 class VisitorsController < ApplicationController
   def index
-    if user_signed_in?
+    if user_signed_in? && Semester.current.present?
       @relationships = Relationship.where(discipline: current_user.disciplines.map {|d| d.id}).where(semester: Semester.current)
       if current_user.student?
         @homeworks = current_user.homeworks.joins(visit: [relationship: :discipline])
@@ -14,6 +14,9 @@ class VisitorsController < ApplicationController
                           .where('disciplines.id IN (?)', current_user.disciplines.select('id'))
                           .order('homeworks.created_at desc')
       end
+    else
+      @relationships = []
+      @homeworks = []
     end
   end
 end
