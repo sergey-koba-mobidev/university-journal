c8a452b0562cd2b36fb3050defc1ae547e960024
Sergey Koba <s.koba@mobidev.biz>
2015-12-12 11:47:34 +0200

    fix homeworks list for teacher and in relationship, remove confirmable

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,7 +18,7 @@ class User < ActiveRecord::Base
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :invitable, :database_authenticatable, :registerable, :confirmable,
+  devise :invitable, :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
   def allowed_disciplines
