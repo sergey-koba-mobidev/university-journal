416603fd64fc7490796600e97ec95ab19ff7a6ce
Sergey Koba <s.koba@mobidev.biz>
2017-03-04 17:00:47 +0200

    downgrade to capistrano 3.4.0

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid only for current version of Capistrano
-lock '3.6.0'
+lock '3.4.0'
 
 set :application, 'UniverJournal'
 set :repo_url, 'git@github.com:sergey-koba-mobidev/university-journal.git'
